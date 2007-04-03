03837d71112fad137174960f0661d159236c8484
etisserant <etisserant@localhost>
2007-04-03 15:22:16 +0200

    Added a new unic canfestival.h file to include in user app. Now, all win32 (vcproj) native projects compile, but don't run.

--- include/unix/applicfg.h
+++ include/unix/applicfg.h
@@ -23,7 +23,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #ifndef __APPLICFG_LINUX__
 #define __APPLICFG_LINUX__
 
-
 #include <string.h>
 #include <stdio.h>
 
--- include/win32/applicfg.h
+++ include/win32/applicfg.h
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #ifndef __APPLICFG_WIN32__
 #define __APPLICFG_WIN32__
 
-#include <timerscfg.h>
+#include <windows.h>
 #include <string.h>
 #include <stdio.h>
 
